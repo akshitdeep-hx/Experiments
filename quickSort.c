95c75559c7bd9a9aecfb304401699e39df781246
akshitdeep-hx <akshitdeeprawat@gmail.com>
2026-09-15 05:49:44 +0000

    commit

---
+++ quickSort.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int a[], int low, int high)
+{
+    int pivot = a[high];
+    int i = low -1;
+
+    for(int j = low; j < high; j++)
+    {
+        if(a[j] <= pivot)
+        {
+            i++;
+            swap(&a[i], &a[j]);
+        }
+    }
+
+    swap(&a[i + 1], &a[high]);
+    return i + 1;
+}
+
+void quickSort(int a[], int low, int high)
+{
+    if(low < high)
+    {
+        int p = partition(a,low,high);
+        quickSort(a, low, p - 1);
+        quickSort(a, p + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter the no of elements:");
+    scanf("%d", &n);
+
+    int a[n];
+    for(int i = 0; i < n; i++)
+    {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+
+    quickSort(a, 0, n-1);
+
+    printf("Sorted Array:");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
